82a08489d5f7381b5049cfd60a5aee74c401a9f0
Francesco <inbox@francescorocca.me>
2026-03-28 02:00:58 +0100

    refactor(main): remove hashmap code

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include "argparser.h"
-#include "hash.h"
 #include "instruction.h"
 
 #include <stdint.h>
@@ -8,32 +7,15 @@
 
 int main(int argc, char **argv) {
 	arguments_s *arguments = {0};
-	// hashmap_s *hashmap = {0};
 
 	arguments = argparse(argc, argv);
 	if (!arguments) {
 		goto cleanup;
 	}
 
-	// hashmap = hm_init();
-	/*if (!hashmap) {
-		goto cleanup;
-	}*/
-
-	fprintf(stdout, "[info] loading definitions schema from %s ...\n", arguments->definitions_schema);
 	fprintf(stdout, "[info] compiling %s ...\n", arguments->file);
 
-	/* Load definitions into instructions hashmap */
-	// load_instructions(arguments->definitions_schema, hashmap);
-
 	assemble_file(arguments->file);
-	/* Retrieve some instructions */
-	/*instruction_s *add = hm_get(hashmap, "add");
-	if (!add) {
-		fprintf(stderr, "[error] 'add' not found.\n");
-	} else {
-		fprintf(stdout, "[debug] 'add': %02x, %02x, %02x\n", add->opcode, add->funct3, add->funct7);
-	}*/
 
 	goto cleanup;
 
@@ -41,6 +23,4 @@ int main(int argc, char **argv) {
 
 cleanup:
 	argparse_free(arguments);
-	// hm_free(hashmap);
-	return EXIT_FAILURE;
 }
