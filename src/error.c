73204c2e9c58d2a4fbafd332d7c221d115382c69
Francesco <inbox@francescorocca.me>
2026-05-13 15:27:11 +0200

    feat(error): add custom error messages

---
+++ src/error.c
@@ -0,0 +1,32 @@
+#include "error.h"
+#include <stddef.h>
+
+#define ARR_SIZE(X) (sizeof(X) / sizeof(*X))
+
+typedef struct message {
+	assembler_error error;
+	const char *message;
+} message;
+
+static const message messages[] = {
+	{ASSEMBLER_OK, "OK"},
+	{ASSEMBLER_ERR, "ERR"},
+	{ASSEMBLER_REGISTER_VALUE, "32 bit registers can't hold such value"},
+	{ASSEMBLER_JUMP_NOT_IMPLEMENTED, "jump is not implemented yet"},
+	{ASSEMBLER_UNKNOWN_PSEUDO, "Unknown pseudo-instruction"},
+	{ASSEMBLER_INVALID_REGISTER, "Invalid register"},
+	{ASSEMBLER_INVALID_IMMEDIATE, "Invalid immediate value"},
+	{ASSEMBLER_PARSE_ERROR, "Failed to parse instruction"},
+	{ASSEMBLER_FILE_ERROR, "Failed to open input file"},
+	{ASSEMBLER_UNKNOWN_INSTRUCTION, "Unknown instruction"},
+};
+
+const char *assembler_error_str(assembler_error err) {
+	for (size_t i = 0; i < ARR_SIZE(messages); ++i) {
+		if (messages[i].error == err) {
+			return messages[i].message;
+		}
+	}
+
+	return "No error message found!";
+}
