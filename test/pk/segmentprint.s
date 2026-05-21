6c460add92ceab113adccc4462af28b3310b9728
Francesco <inbox@francescorocca.me>
2026-05-21 18:40:15 +0200

    test(segment): revert

--- test/pk/segmentprint.s
+++ test/pk/segmentprint.s
@@ -1,13 +1,13 @@
 # segments print example (using spike and pk)
 
 .data
-msg: .asciiz "SEMITO-V"
+msg: .asciiz "SEMITO-V\n"
 
 .text
 li a7, 64 # sys_write
 li a0, 1 # stdout
 la a1, msg # load msg address into a1
-li a2, 9 # length
+li a2, 10 # length
 ecall
 
 li a0, 0 # return code
